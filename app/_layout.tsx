99a54dd7134e4146e3ecab4e9653a5ce0b60e11a
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-07-25 17:11:13 +0530

    minor update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,30 +5,22 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Toaster } from 'sonner-native';
 
-const convex = new ConvexReactClient("https://ideal-penguin-725.convex.cloud"); 
-
-function RootStack() {
-  return (
-    <>
-      <Stack screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Home" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
-  );
-}
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL);
 
 export default function App() {
   return (
     <ConvexProvider client={convex}>
       <SafeAreaProvider style={styles.container}>
         <Toaster />
-        {/* <Navi0gationContainer> */}
-        <RootStack />
-        {/* </NavigationContainer> */}
+        <Stack
+          screenOptions={{
+            headerShown: false
+          }}
+          initialRouteName='Home'>
+          <Stack.Screen name="Home" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
       </SafeAreaProvider>
     </ConvexProvider>
   );
